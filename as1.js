ac66f964b9da057547058f0bc5fec1d41376756b
RupaKandepi <125371358+RupaKandepi@users.noreply.github.com>
2023-12-21 12:33:07 +0530

    Add files via upload

---
+++ as1.js
@@ -0,0 +1,18 @@
+/*let arr=[9,'rup',{tech:'JS'},()=>console.log("hello")
+]
+//console.log(arr);
+arr[3]();
+arr[4]=function(user){
+    console.log("hi "+user);
+}
+arr[4]('man');
+arr.pop();
+console.log(arr.length);
+arr.p*/
+let array=[1,2,3,4,5,6]
+array.push(7,8);
+console.log(array);
+//array.unshift(0,1)
+//console.log(array);
+ console.log(array.splice(3,2,33,44,55,66));
+console.log(array);
